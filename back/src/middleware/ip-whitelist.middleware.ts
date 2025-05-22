a161e1aae70a27e41b594b0d8d12f5338f07743e
ega666881 <63003334+ega666881@users.noreply.github.com>
2025-05-22 13:33:53 +0300

    1

--- back/src/middleware/ip-whitelist.middleware.ts
+++ back/src/middleware/ip-whitelist.middleware.ts
@@ -3,18 +3,34 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LocalhostOnlyMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    const allowedIps = ['127.0.0.1', '::1'];
-
-    // Получаем IP клиента
-    const ip = req.ip || req.connection.remoteAddress;
-    console.log(ip)
-    if (!ip || !allowedIps.includes(ip.trim())) {
-      return res.status(403).json({
-        message: 'Доступ запрещён: разрешены только запросы с localhost',
-      });
+    getClientIp(req) {
+        const forwardedIps = req.headers['x-forwarded-for'];
+        if (forwardedIps) {
+          return forwardedIps.split(',')[0].trim();
+        }
+      
+        let ip = req.ip || req.connection.remoteAddress;
+      
+        
+        if (ip.startsWith('::ffff:')) {
+          ip = ip.substring(7);
+        }
+      
+        return ip.trim();
     }
+    
+    use(req: Request, res: Response, next: NextFunction) {
+        const allowedIps = ['127.0.0.1', '::1'];
 
-    next();
-  }
+        // Получаем IP клиента
+        const ip = this.getClientIp(req);
+        console.log(ip)
+        if (!ip || !allowedIps.includes(ip.trim())) {
+        return res.status(403).json({
+            message: 'Доступ запрещён: разрешены только запросы с localhost',
+        });
+        }
+
+        next();
+    }
 }
