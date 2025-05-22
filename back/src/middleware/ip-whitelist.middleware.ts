c75f8b9d32b39468f7b02ee38be9926efe5547a5
ega666881 <63003334+ega666881@users.noreply.github.com>
2025-05-22 13:34:56 +0300

    1

--- back/src/middleware/ip-whitelist.middleware.ts
+++ back/src/middleware/ip-whitelist.middleware.ts
@@ -1,29 +1,31 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
+function getClientIp(req) {
+    const forwardedIps = req.headers['x-forwarded-for'];
+    if (forwardedIps) {
+      return forwardedIps.split(',')[0].trim();
+    }
+  
+    let ip = req.ip || req.connection.remoteAddress;
+  
+    
+    if (ip.startsWith('::ffff:')) {
+      ip = ip.substring(7);
+    }
+  
+    return ip.trim();
+}
+
 @Injectable()
 export class LocalhostOnlyMiddleware implements NestMiddleware {
-    getClientIp(req) {
-        const forwardedIps = req.headers['x-forwarded-for'];
-        if (forwardedIps) {
-          return forwardedIps.split(',')[0].trim();
-        }
-      
-        let ip = req.ip || req.connection.remoteAddress;
-      
-        
-        if (ip.startsWith('::ffff:')) {
-          ip = ip.substring(7);
-        }
-      
-        return ip.trim();
-    }
+    
     
     use(req: Request, res: Response, next: NextFunction) {
         const allowedIps = ['127.0.0.1', '::1'];
 
         // Получаем IP клиента
-        const ip = this.getClientIp(req);
+        const ip = getClientIp(req);
         console.log(ip)
         if (!ip || !allowedIps.includes(ip.trim())) {
         return res.status(403).json({
