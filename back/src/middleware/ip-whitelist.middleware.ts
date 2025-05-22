a8967e597447519af1a3846fe44946fdd8b51177
ega666881 <63003334+ega666881@users.noreply.github.com>
2025-05-22 13:20:28 +0300

    1

--- back/src/middleware/ip-whitelist.middleware.ts
+++ back/src/middleware/ip-whitelist.middleware.ts
@@ -5,12 +5,12 @@ const ALLOWED_IPS = ['127.0.0.1', '::1'];
 
 export function IpWhitelistMiddleware(req: Request, res: Response, next: NextFunction) {
     const clientIp = req.ip || req.connection.remoteAddress;
-    
+    console.log(clientIp)
     // Если используется прокси (например, Nginx), используй x-forwarded-for
     const forwardedIps = req.headers['x-forwarded-for'];
-    
+    console.log(forwardedIps)
     const ip = forwardedIps ? (Array.isArray(forwardedIps) ? forwardedIps[0] : forwardedIps.split(',')[0]) : clientIp;
-   
+    console.log(ip)
     if (!ip) {
         return res.status(403).json({ message: 'IP address not found' });
     }
