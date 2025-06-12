1d5d1a68c3b787d8b50d141b4bc55e38734c80a0
ega666881 <63003334+ega666881@users.noreply.github.com>
2025-06-12 17:17:05 +0300

    1

--- front/src/pages/profilePage/balanceOperations.jsx
+++ front/src/pages/profilePage/balanceOperations.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Typography, Avatar, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import mediaManager from '../../utils/mediaManager';
 import { observer } from 'mobx-react';
 import clientStore from '../../stores/clientStore';
 import createTransactionModalStore from '../../stores/createTransactionModalStore';
+import { useTonConnectUI } from '@tonconnect/ui-react';
 
 function BalanceOperations() {
+    const [connected, setConnected] = useState(false)
+    const [tonConnectUI, setOptions] = useTonConnectUI()
     const navigate = useNavigate()
+    
+    useEffect(() => {
+        const unsubscribe = tonConnectUI.onStatusChange(wallet => {
+          setConnected(!!wallet);
+          console.log(wallet)
+          
+        });
+    
+        return () => {
+          unsubscribe();
+        };
+      }, []);
     return (
         <Box
             sx={{
@@ -30,6 +46,7 @@ function BalanceOperations() {
                     borderRadius: 27
                 }}
                 onClick={() => createTransactionModalStore.setOpenModal(true)}
+                disabled={!connected}
             >
                 Пополнить +
             </Button>
@@ -42,6 +59,7 @@ function BalanceOperations() {
                     borderColor: '#878787',
                     borderRadius: 27
                 }}
+                disabled={!connected}
             >
                 Вывести
             </Button>
